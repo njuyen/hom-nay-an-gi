433216e9eb8b863019be467032ec015956130ea0
nguyenphuocpham <njuyen@gmail.com>
2021-12-24 00:42:27 +0700

    Add models

--- sitevm/menu/models.py
+++ sitevm/menu/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+class Dish(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
